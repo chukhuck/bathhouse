84b055dd00a12ce167c439fc10917a86a6631a77
Кузнецов Александр <aka.kuznec@ya.ru>
2020-12-24 14:23:25 +0300

    Add OfficeController. Delete boilerplate controller

---
+++ infrastructure/Bathhouse.Api/Controllers/OfficeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bathhouse.Api.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class OfficeController : ControllerBase
+  {
+    private IOfficeRepository _officeRepository;
+
+    private readonly ILogger<OfficeController> _logger;
+
+    public OfficeController(ILogger<OfficeController> logger, IOfficeRepository office)
+    {
+      _logger = logger;
+      _officeRepository = office;
+    }
+
+    [HttpGet]
+    public IEnumerable<Office> Get()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
--- infrastructure/Bathhouse.Api/Startup.cs
+++ infrastructure/Bathhouse.Api/Startup.cs
@@ -32,6 +32,9 @@ namespace Bathhouse.Api
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Bathhouse.Api", Version = "v1" });
       });
+
+
+      
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
