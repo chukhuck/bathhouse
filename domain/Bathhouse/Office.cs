6cfce458cd1361926a9b3efe644fa506db923ca5
Кузнецов Александр <aka.kuznec@ya.ru>
2020-12-24 17:43:06 +0300

    Add extra props to Office

--- domain/Bathhouse/Office.cs
+++ domain/Bathhouse/Office.cs
@@ -2,8 +2,12 @@
 
 namespace Bathhouse
 {
-    public class Office : Entity
-    {
-        public int Number { get; set; }
-    }
+  public class Office : Entity
+  {
+    public int Number => 0;
+    public string Adress => "Москва, ";
+    public string Phone => "+7-495-XXX-XX-XX";
+    public DateTime TimeOfOpen => DateTime.MinValue.AddHours(8).AddMinutes(0);
+    public DateTimeOffset TimeOfClose => DateTime.MinValue.AddHours(22).AddMinutes(0);
+  }
 }
