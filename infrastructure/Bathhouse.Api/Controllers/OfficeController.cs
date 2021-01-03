c1254e95588391fdd4d7d37d72b735a250575b18
Кузнецов Александр <aka.kuznec@ya.ru>
2021-01-03 17:57:27 +0300

    Move CRUD method of controllers to generic RichControllerBase

--- infrastructure/Bathhouse.Api/Controllers/OfficeController.cs
+++ infrastructure/Bathhouse.Api/Controllers/OfficeController.cs
@@ -11,58 +11,14 @@ using System.Threading.Tasks;
 
 namespace Bathhouse.Api.Controllers
 {
-  [ApiController]
   [Route("[controller]")]
-  public class OfficeController : ControllerBase
+  public class OfficeController : RichControllerBase<Office, OfficeModel>
   {
-    private ICRUDRepository<Office> _officeRepository;
-
-    private readonly ILogger<OfficeController> _logger;
-
-    private readonly IMapper _mapper;
-
-    public OfficeController(ILogger<OfficeController> logger, IMapper mapper, ICRUDRepository<Office> officeRepository)
+    public OfficeController(ILogger<RichControllerBase<Office, OfficeModel>> logger, IMapper mapper, ICRUDRepository<Office> repository) 
+      : base(logger, mapper, repository)
     {
-      _logger = logger;
-      _officeRepository = officeRepository;
-      _mapper = mapper;
     }
 
-    [HttpGet]
-    public IEnumerable<OfficeModel> Get()
-    {
-      return _mapper.Map<IEnumerable<Office>, IEnumerable<OfficeModel>>(_officeRepository.GetAll());
-    }
 
-    [HttpGet]
-    [Route("{id:guid}")]
-    public OfficeModel GetById(Guid id)
-    {
-      return _mapper.Map<Office, OfficeModel>(_officeRepository.Get(id));
-    }
-
-    [HttpPost]
-    public IActionResult Add(OfficeModel office)
-    {
-      _officeRepository.Create(_mapper.Map<OfficeModel, Office>(office));
-
-      return Ok();
-    }
-
-    [HttpPut]
-    public IActionResult Update(OfficeModel office)
-    {
-      _officeRepository.Update(_mapper.Map<OfficeModel, Office>(office));
-
-      return Ok();
-    }
-
-    [HttpDelete]
-    public IActionResult Delete(Guid id)
-    {
-      _officeRepository.Delete(id);
-
-      return Ok();
-    }
   }
 }
---
+++ infrastructure/Bathhouse.Api/Controllers/RichControllerBase.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Bathhouse.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Bathhouse.Api.Controllers
+{
+
+  [ApiController]
+  public class RichControllerBase<TEntity, TEntityModel> : ControllerBase
+    where TEntity: Entity 
+    where TEntityModel : class  
+  {
+    protected readonly ICRUDRepository<TEntity> _repository;
+
+    protected readonly ILogger<RichControllerBase<TEntity, TEntityModel>> _logger;
+
+    protected readonly IMapper _mapper;
+
+    public RichControllerBase(ILogger<RichControllerBase<TEntity, TEntityModel>> logger, IMapper mapper, ICRUDRepository<TEntity> repository)
+    {
+      _logger = logger;
+      _repository = repository;
+      _mapper = mapper;
+      
+    }
+
+    [HttpGet]
+    public IEnumerable<TEntityModel> Get()
+    {
+      return _mapper.Map<IEnumerable<TEntity>, IEnumerable<TEntityModel>>(_repository.GetAll());
+    }
+
+    [HttpGet]
+    [Route("{id:guid}")]
+    public TEntityModel GetById(Guid id)
+    {
+      return _mapper.Map<TEntity, TEntityModel>(_repository.Get(id));
+    }
+
+    [HttpPost]
+    public IActionResult Add(TEntityModel office)
+    {
+      _repository.Create(_mapper.Map<TEntityModel, TEntity>(office));
+
+      return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult Update(TEntityModel office)
+    {
+      _repository.Update(_mapper.Map<TEntityModel, TEntity>(office));
+
+      return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(Guid id)
+    {
+      _repository.Delete(id);
+
+      return Ok();
+    }
+  }
+}
