8318e17b5bc1ecebb18c534f21415c19d18613c2
Кузнецов Александр <aka.kuznec@ya.ru>
2021-01-03 22:44:49 +0300

    Add new type of entity <Employee>

---
+++ domain/Bathhouse/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bathhouse.Entities
+{
+  public class Employee : Entity
+  {
+    public string LastName { get; set; } = "Фамилия";
+  }
+}
---
+++ domain/Bathhouse/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bathhouse.Models
+{
+  public class EmployeeModel : EntityModel
+  {
+    [Required]
+    [DefaultValue("Фамилия")]
+    public string LastName { get; set; } = "Фамилия";
+  }
+}
