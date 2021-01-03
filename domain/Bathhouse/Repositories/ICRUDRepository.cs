149a9c2345e04bb4e7c3157da309c224981b0197
Кузнецов Александр <aka.kuznec@ya.ru>
2021-01-03 14:14:16 +0300

    Add ICRUDRepository

---
+++ domain/Bathhouse/Repositories/ICRUDRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bathhouse.Repositories
+{
+  public interface ICRUDRepository<TEntity> where TEntity : Entity
+  {
+    public IEnumerable<TEntity> GetAll();
+    public TEntity Get(Guid id);
+    public TEntity Create(TEntity model);
+    public TEntity Update(TEntity model);
+    public void Delete(Guid id);
+  }
+}
