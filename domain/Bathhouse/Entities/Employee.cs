a25c4f53cafaf66f70dae279bcc42d6c9c720d1b
Кузнецов Александр <aka.kuznec@ya.ru>
2021-01-05 13:59:03 +0300

    Add data annotation to Employee entity

--- domain/Bathhouse/Entities/Employee.cs
+++ domain/Bathhouse/Entities/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,30 @@ namespace Bathhouse.Entities
 {
   public class Employee : Entity
   {
+    [Required]
+    [DataType(DataType.Text)]
+    [MaxLength(25, ErrorMessage = "Maximum field length exceeded. Max lenght of field is 25 symbols.")]
     public string LastName { get; set; } = "Фамилия";
+
+    [DataType(DataType.Text)]
+    [MaxLength(25, ErrorMessage = "Maximum field length exceeded. Max lenght of field is 25 symbols.")]
+    public string FirstName { get; set; } = "Имя";
+
+    [Phone(ErrorMessage = "Incorrect phone format.")]
+    public string Phone { get; set; } = "+7-495-000-00-00";
+
+    [DataType(DataType.Date, ErrorMessage = "Incorrect date format.")]
+    public DateTime DoB { get; set; } = DateTime.Parse("1950-01-01");
+
+    public EmployeeType Type { get; set; } = EmployeeType.Manager;
+  }
+
+
+  public enum EmployeeType
+  {
+    Director,
+    Manager,
+    Employee,
+    TEchnicalSupport
   }
 }
--- domain/Bathhouse/Models/EmployeeModel.cs
+++ domain/Bathhouse/Models/EmployeeModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bathhouse.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -10,8 +11,41 @@ namespace Bathhouse.Models
 {
   public class EmployeeModel : EntityModel
   {
+    /// <summary>
+    /// LastName of employee
+    /// </summary>
     [Required]
+    [DataType(DataType.Text)]
+    [MaxLength(25, ErrorMessage = "Maximum field length exceeded. Max lenght of field is 25 symbols.")]
     [DefaultValue("Фамилия")]
     public string LastName { get; set; } = "Фамилия";
+
+    /// <summary>
+    /// FirstName of employee
+    /// </summary>
+    [DataType(DataType.Text)]
+    [MaxLength(25, ErrorMessage = "Maximum field length exceeded. Max lenght of field is 25 symbols.")]
+    [DefaultValue("Имя")]
+    public string FirstName { get; set; } = "Имя";
+
+    /// <summary>
+    /// Phone of employee
+    /// </summary>
+    [Phone(ErrorMessage = "Incorrect phone format.")]
+    [DefaultValue("+7-495-000-00-00")]
+    public string Phone { get; set; } = "+7-495-000-00-00";
+
+    /// <summary>
+    /// Day of Birth
+    /// </summary>
+    [DataType(DataType.Date, ErrorMessage = "Incorrect date format.")]
+    [DefaultValue("1950-01-01")]
+    public DateTime DoB { get; set; } = DateTime.Parse("1950-01-01");
+
+    /// <summary>
+    /// Type of employee
+    /// </summary>
+    [DefaultValue(EmployeeType.Manager)]
+    public EmployeeType Type { get; set; } = EmployeeType.Manager;
   }
 }
