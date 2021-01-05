e460d10af4576bcdefa992185d9b52e7b7a70b6f
Кузнецов Александр <aka.kuznec@ya.ru>
2021-01-05 13:34:40 +0300

    Add data annotation to Entity

--- domain/Bathhouse/Entities/Entity.cs
+++ domain/Bathhouse/Entities/Entity.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bathhouse
 {
   public class Entity
   {
+    [Required]
+    [Key()]
     public Guid Id { get; set; } = Guid.NewGuid();
   }
 }
