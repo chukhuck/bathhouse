61aeaefaeb3c0ae97bad85dab369219b9aae4794
Кузнецов Александр <aka.kuznec@ya.ru>
2021-01-10 21:44:48 +0300

    Add action DeleteManager

--- infrastructure/Bathhouse.Api/Controllers/OfficeController.cs
+++ infrastructure/Bathhouse.Api/Controllers/OfficeController.cs
@@ -23,15 +23,15 @@ namespace Bathhouse.Api.Controllers
     /// <summary>
     /// Get manager of office with ID
     /// </summary>
-    /// <param name="id">The entity ID</param>
+    /// <param name="id">The Office ID</param>
     /// <response code="404">Office with current ID is not found of there is no manager at the office with current ID</response>
-    /// <response code="200">Getting entity is successul</response>
+    /// <response code="200">Getting manager is successul</response>
     /// <response code="500">Exception on server side was fired</response>
     [HttpGet()]
     [Route("{id:guid}/manager")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType((int)StatusCodes.Status404NotFound)]
-    public virtual ActionResult<EmployeeResponse> GetManager(Guid id)
+    public ActionResult<EmployeeResponse> GetManager(Guid id)
     {
       try
       {
@@ -56,6 +56,42 @@ namespace Bathhouse.Api.Controllers
       {
         _logger.LogError($"While getting manager of office id={id} an exception was fired. Exception: {ex.Data}. Inner ex: {ex.InnerException}");
         return StatusCode(StatusCodes.Status500InternalServerError, $"While getting manager of office id={id} an exception was fired");
+      }
+    }
+
+    /// <summary>
+    /// Delete manager of office with ID
+    /// </summary>
+    /// <param name="id">Office ID</param>
+    /// <response code="404">Office with current ID is not found</response>
+    /// <response code="204">Deleting manager is successul</response>
+    /// <response code="500">Exception on server side was fired</response>
+    [HttpDelete]
+    [Route("{id:guid}/manager")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType((int)StatusCodes.Status400BadRequest)]
+    [ProducesResponseType((int)StatusCodes.Status404NotFound)]
+    public virtual IActionResult DeleteManager(Guid id)
+    {
+      try
+      {
+        if (!_repository.Exist(id))
+        {
+          _logger.LogInformation($"Office with ID={id} was not found.");
+          return NotFound($"Office with ID={id} was not found.");
+        }
+
+        var office =_repository.Get(id);
+        office.Manager = null;
+        office.ManagerId = null;
+        _logger.LogInformation($"Manager of office id={id} was deleted successfully.");
+
+        return NoContent();
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError($"While deleting manager for office  id={id} an exception was fired. Exception: {ex.Data}. Inner ex: {ex.InnerException}");
+        return StatusCode(StatusCodes.Status500InternalServerError, $"While deleting manager for office  id={id}  an exception was fired");
       }
     }
   }
