6854393f16ffcaae3ba659982d72072a563417a5
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-01-25 00:00:31 +0300

    test: Add checking to Method ToList with no answers

---
+++ tests/Bathhouse.Test/SurveyResultTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Bathhouse.Entities;
+using Bathhouse.Memory;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Bathhouse.Test
+{
+  public class SurveyResultTest
+  {
+    public static readonly SurveyResult surveyResult = InMemoryContext.SurveyResults.FirstOrDefault();
+
+    [Fact]
+    public void To_List_With_No_Answers()
+    {
+      surveyResult.Answers = new List<Answer>();
+
+      List<string> row = new List<string>();
+
+      row.Add(surveyResult.CreationDate.ToString());
+      row.Add(surveyResult.Author.LastName);
+
+      Assert.Equal(row, surveyResult.ToList());
+    }
+  }
+}
