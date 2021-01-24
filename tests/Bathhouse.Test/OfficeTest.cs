d7d78e5d27ed6596257ac1a35f6d53c1d1355bff
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-01-25 00:07:33 +0300

    test: Add test for GetManagers

---
+++ tests/Bathhouse.Test/OfficeTest.cs
@@ -0,0 +1,22 @@
+﻿using Bathhouse.Entities;
+using Bathhouse.Memory;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Bathhouse.Test
+{
+  public class OfficeTest
+  {
+    public static readonly Office office = InMemoryContext.Offices.FirstOrDefault();
+
+    [Fact]
+    public void GetManagers()
+    {
+      List<Employee> managers = office.Employees.Where(e => e.Type == EmployeeType.Manager).ToList();
+
+      Assert.Equal(managers, office.GetManagers());
+    }
+  }
+}
---
+++ tests/Bathhouse.Test/SurveyTest.cs
@@ -0,0 +1,13 @@
+﻿using Bathhouse.Entities;
+using Bathhouse.Memory;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Bathhouse.Test
+{
+  public class SurveyTest
+  {
+  }
+}
