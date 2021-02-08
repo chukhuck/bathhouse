59d9a728b992a6d67f7f5fb294091be8425d0bb6
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-02-08 12:04:05 +0300

    test: Add test for creating Question with default values

---
+++ tests/Bathhouse.Test/QuestionTest.cs
@@ -0,0 +1,21 @@
+﻿using Bathhouse.Entities;
+using Bathhouse.ValueTypes;
+using System;
+using Xunit;
+
+namespace Bathhouse.Test
+{
+  public class QuestionTest
+  {
+    [Fact]
+    public void Create_Question_With_Default_Values()
+    {
+      Question question = new();
+
+      Assert.False(question.IsKey);
+      Assert.Equal("New question", question.Text);
+      Assert.Equal("Newquestion", question.Name);
+      Assert.Equal(QuestionType.Number, question.Type);
+    }
+  }
+}
