2d23dc56765e8fba33efcf64f646feb83da91290
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-02-08 12:14:09 +0300

    test: Add test for creating WorkItem with default values

---
+++ tests/Bathhouse.Test/WorkItemTest.cs
@@ -0,0 +1,26 @@
+﻿using Bathhouse.Entities;
+using Bathhouse.ValueTypes;
+using System;
+using Xunit;
+
+namespace Bathhouse.Test
+{
+  public class WorkItemTest
+  {
+    [Fact]
+    public void Create_Employee_With_Default_Values()
+    {
+      WorkItem workItem = new();
+
+      Assert.False(workItem.IsImportant);
+      Assert.False(workItem.IsUrgent);
+
+      Assert.Equal(string.Empty, workItem.Description);
+      Assert.Equal(WorkItemStatus.Created, workItem.Status);
+
+      Assert.Equal(DateTime.Now.Date, workItem.CreationDate.Date);
+      Assert.Equal(DateTime.Now.Date, workItem.StartDate.Date);
+      Assert.Equal(DateTime.Now.AddDays(1).Date, workItem.EndDate.Date);
+    }
+  }
+}
