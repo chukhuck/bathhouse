914dac155a3f718b7636bb1256ebbcccd5c6e641
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-02-08 11:40:27 +0300

    test: Create test for Answer Entity

---
+++ tests/Bathhouse.Test/AnswerTest.cs
@@ -0,0 +1,21 @@
+﻿using Bathhouse.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bathhouse.Test
+{
+  public class AnswerTest
+  {
+    [Fact]
+    public void Create_Answer_With_Value_Equal_String_Empty()
+    {
+      Answer answer = new();
+
+      Assert.Equal(string.Empty, answer.Value);
+    }
+  }
+}
