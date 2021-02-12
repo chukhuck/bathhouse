34ce491c9116a87fb6f22ed27f84f4b49dec4fe6
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-02-12 15:32:12 +0300

    feat: Add generator for Fibonacci sequence

---
+++ src/infrastructure/Chuk.Helpers/Sequence/Fibonacci.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chuk.Helpers
+{
+  public static partial class Sequence
+  {
+    public static IEnumerable<int> Fibonacci(int count)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
