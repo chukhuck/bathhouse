b7d9dbb06827a5c0b9ac86b8d5ac1c2bd48ba797
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-03-07 20:31:11 +0300

    refact: Separate Filters and FilterQueries

---
+++ src/infrastructure/Bathhouse.Api/Common/Filters/SurveyFilter.cs
@@ -0,0 +1,25 @@
+﻿using Bathhouse.Contracts.v1.Models.Queries;
+using Bathhouse.Entities;
+using Chuk.Helpers.Expressions;
+using System;
+using System.Linq.Expressions;
+
+namespace Bathhouse.Api.Common.Filters
+{
+  public class SurveyFilter
+  {
+    private readonly SurveyFilterQuery query;
+    public SurveyFilter(SurveyFilterQuery query)
+    {
+      this.query = query;
+    }
+
+    public Expression<Func<Survey, bool>> Compose()
+    {
+      Expression<Func<Survey, bool>> filterByAuthor = query?.AuthorId == null ? wi => true : wi => wi.AuthorId == query.AuthorId;
+      Expression<Func<Survey, bool>> filterByStatus = query?.Status == null ? wi => true : wi => wi.Status == query.Status;
+
+      return filterByAuthor.And(filterByStatus);
+    }
+  }
+}
---
+++ src/infrastructure/Bathhouse.Api/Common/Filters/WorkItemFilter.cs
@@ -0,0 +1,27 @@
+﻿using Bathhouse.Contracts.v1.Models.Queries;
+using Bathhouse.Entities;
+using Chuk.Helpers.Expressions;
+using System;
+using System.Linq.Expressions;
+
+namespace Bathhouse.Api.Common.Filters
+{
+  public class WorkItemFilter
+  {
+    private readonly WorkItemFilterQuery query;
+
+    public WorkItemFilter(WorkItemFilterQuery query)
+    {
+      this.query = query;
+    }
+
+    public Expression<Func<WorkItem, bool>> Compose()
+    {
+      Expression<Func<WorkItem, bool>> filterByCreator = query?.CreatorId == null ? wi => true : wi => wi.CreatorId == query.CreatorId;
+      Expression<Func<WorkItem, bool>> filterByExecutor = query?.ExecutorId == null ? wi => true : wi => wi.ExecutorId == query.ExecutorId;
+      Expression<Func<WorkItem, bool>> filterByStatus = query?.Status == null ? wi => true : wi => wi.Status == query.Status;
+
+      return filterByCreator.And(filterByExecutor).And(filterByStatus);
+    }
+  }
+}
